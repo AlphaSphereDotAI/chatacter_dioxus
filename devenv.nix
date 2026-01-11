d4afdf11566a21ff923f7757599da43677fe322a
Mohamed Hisham Abdelzaher <mohamed.hisham.abdelzaher@gmail.com>
2026-01-11 22:49:52 +0200

    Include yamllint configuration in devenv.nix
    Add custom yamllint configuration to devenv.nix

--- devenv.nix
+++ devenv.nix
@@ -7,6 +7,18 @@
 }:
 
 {
+  files = {
+    ".yamllint.yaml".yaml = {
+      extends = "default";
+      rules = {
+        document-start = "disable";
+        truthy = "disable";
+        comments = "disable";
+        line-length.max = 120;
+      };
+    };
+  };
+
   # https://devenv.sh/basics/
   env.GREET = "devenv";
 
